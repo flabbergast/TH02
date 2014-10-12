1d7e29838f19dac845bc29357c1100ccd26ae45d
flabbergast <s3+flabbergast@sdfeu.org>
2014-10-12 20:40:14 +0100

    JNu test code working.

--- examples/test_th02_t84_softi2c/test_th02_t84_softi2c.ino
+++ examples/test_th02_t84_softi2c/test_th02_t84_softi2c.ino
@@ -1,7 +1,23 @@
 #if not defined(__AVR_ATtiny84__)
-    #error "This one is for attiny84."
+    #error "This one is for attiny84. Sorry."
 #endif
 
+/* 
+ * Sample code for JeeNode Micro + TH02 (software I2C)
+ * (c) 2014 flabbergast
+ *
+ * Comments:
+ *  - the current code is running out of memory (I think), so can't compute compensated
+ *    humidity on the attiny, the compensation needs to be done at the receiving end.
+ *  - couldn't use Sleepy for sleep (same reason, memory)
+ *  - if you want to use this for a real sensor, you should also power the TH02 sensor
+ *    a pin and power it up/down as needed, instead of having it powered continuously.
+ *    According to the datasheet, the device doesn't have a sleep mode, and consumes
+ *    150uA of power in standby (too much). Also, note that it can guzzle up to 40mA
+ *    of power for 5ms on each powerup and shutdown...
+ *  - I would guess that one can save memory by getting rid of the C++ Object stuff and
+ *    coding the I2C transactions directly here. That's the next step.
+ */
 
 #define RF69_COMPAT 1
 #define RF_NODE_ID 22
@@ -25,7 +41,6 @@ TH02 sensor;
 typedef struct {                            // RFM12B RF payload datastructure
       int16_t temp;
       int16_t rh;
-      int16_t rh_comp;
 } Payload;
 Payload payload;
 
@@ -47,61 +62,38 @@ void setup() {
   // send an echo that we're starting  
   payload.temp = 1;
   payload.rh = 2;
-  payload.rh_comp = 3;
   rf12_sendNow(0, &payload, sizeof payload);
   rf12_sendWait(2);
-//  rf12_sleep(RF12_SLEEP);
 
-//  i2c_init();
+  // initialise i2c
   sensor.init();
-  
-  sensor.startTempConv();
-  sensor.waitEndConversion();
-
-  payload.temp = sensor.getConversionValue();
-
-  sensor.startRHConv();
-  sensor.waitEndConversion();
-
-  payload.rh = sensor.getConversionValue();
-  payload.rh_comp = sensor.getConpensatedRH(true);
-
-//  payload.three = 0xF0;
-//  rf12_sleep(RF12_WAKEUP);
-//  rf12_sendNow(0, &payload, sizeof payload);
-//  rf12_sendWait(2);
-//  rf12_sleep(RF12_SLEEP);
-
-//  payload.one = i2c_start( 0x80 );
-//  payload.two = i2c_write( 17 );
-//  i2c_rep_start( 0x81 );
-//  payload.three = i2c_read(true) >> 4;
-//  i2c_stop();
-
-//  rf12_sleep(RF12_WAKEUP);
-  rf12_sendNow(0, &payload, sizeof payload);
-  rf12_sendWait(2);
-//  rf12_sleep(RF12_SLEEP);
-  
-//  i2c_start( 0x80 );
-//  payload.two = i2c_write( 0 );
-//  i2c_rep_start( 0x81 );
-//  payload.three = i2c_read(true);
-//  i2c_stop();
-//  rf12_sendNow(0, &payload, sizeof payload);
-//  rf12_sendWait(2);
-//  
-//  payload.one = i2c_start( 0x80 );
-//  payload.two = i2c_write( 3 );
-//  i2c_rep_start( 0x81 );
-//  payload.three = i2c_read(true);
-//  i2c_stop();
-//  rf12_sendNow(0, &payload, sizeof payload);
-//  rf12_sendWait(2);
-
 }
 
 void loop()
 {
+  // do a temp conversion
+  sensor.startTempConv();
+  sensor.waitEndConversion();
+
+  // read the result
+  payload.temp = sensor.getConversionValue_nomath();
+
+  // do a humidity conversion
+  sensor.startRHConv();
+  sensor.waitEndConversion();
+
+  // read the result
+  payload.rh = sensor.getConversionValue_nomath();
+  
+  // couldn't get the math code working on attiny84 (I think a problem with memory size)
+//  payload.rh_comp = sensor.getConpensatedRH(false);
+
+  // send the reading
+  rf12_sendNow(0, &payload, sizeof payload);
+  rf12_sendWait(2);
+  
+  // running out of memory again: can't use Sleepy
+//  Sleepy::loseSomeTime(20000); // JeeLabs power save function: enter low power mode for x seconds (valid range 16-65000 ms)
+
   delay(20000);
 }
